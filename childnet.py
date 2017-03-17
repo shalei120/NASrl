7ac09a3d3d7966ef8c782497d2370cc3b1ba8505
shalei120 <shalei120@sina.com>
2017-03-17 16:17:21 +0800

    update

--- childnet.py
+++ childnet.py
@@ -2,22 +2,23 @@ import numpy as np
 import cPickle as pickle
 import tensorflow as tf
 import time
-#%matplotlib inline
+# %matplotlib inline
 import matplotlib.pyplot as plt
 import math
 
+
 def load_data():
-    f = open('../data/XY_oneclick_np_100','rb')
-    #x = np.asarray(x)
-    #y = np.asarray(y)
+    f = open('../data/XY_oneclick_np_100', 'rb')
+    # x = np.asarray(x)
+    # y = np.asarray(y)
 
-    #features = []
+    # features = []
 
-    x=pickle.load( f)
-    y=pickle.load( f)
+    x = pickle.load(f)
+    y = pickle.load(f)
     features = pickle.load(f)
 
-    new_x = [[0 if fea is None else fea  for fea in case] for case in x]
+    new_x = [[0 if fea is None else fea for fea in case] for case in x]
     x = np.asarray(new_x)
 
     indices = range(len(y))
@@ -25,8 +26,6 @@ def load_data():
 
     x = x[indices]
     y = y[indices]
-
-
     '''fout = open('../data/XY.txt', 'w')
     for i in x:
         for j in i:
@@ -35,10 +34,7 @@ def load_data():
 
     fout.close()'''
 
-
-
-
-    train_size = int(len(x)*0.7)
+    train_size = int(len(x) * 0.7)
     dev_size = int(len(x) * 0.3)
 
     train_x = x[:train_size]
@@ -49,7 +45,8 @@ def load_data():
     test_y = y[train_size + dev_size:]
     return train_x, train_y, dev_x, dev_y, test_x, test_y, features
 
-def predict(sess,childinput_x, childinput_y, childaccuracy, datax, datay, batchnum):
+
+def predict(sess, childinput_x, childinput_y, childaccuracy, datax, datay, batchnum):
     childvalid_acc = 0.0
 
     for i in range(batchnum):
@@ -58,35 +55,74 @@ def predict(sess,childinput_x, childinput_y, childaccuracy, datax, datay, batchn
             childinput_y: datay[i * childbatch_size: (i + 1) * childbatch_size]
         })
         childvalid_acc += child_np_accuracy
-        #print child_np_accuracy
+        # print child_np_accuracy
 
     childvalid_acc = 1.0 * childvalid_acc / len(datay)
     return childvalid_acc
 
+
 train_x, train_y, dev_x, dev_y, test_x, test_y, features = load_data()
 
+
 def variable_summaries(var):
-  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-  with tf.name_scope('summaries'):
-    mean = tf.reduce_mean(var)
-    tf.summary.scalar('mean', mean)
-    with tf.name_scope('stddev'):
-      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-    tf.summary.scalar('stddev', stddev)
-    tf.summary.scalar('max', tf.reduce_max(var))
-    tf.summary.scalar('min', tf.reduce_min(var))
-    tf.summary.histogram('histogram', var)
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+
+def weight_variable(shape):
+    """Create a weight variable with appropriate initialization."""
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    """Create a bias variable with appropriate initialization."""
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.tanh):
+    """Reusable code for making a simple neural net layer.
+    It does a matrix multiply, bias add, and then uses relu to nonlinearize.
+    It also sets up name scoping so that the resultant graph is easy to read,
+    and adds a number of summary ops.
+    """
+    # Adding a name scope ensures logical grouping of the layers in the graph.
+    with tf.name_scope(layer_name):
+        # This Variable will hold the state of the weights for the layer
+        with tf.name_scope('weights'):
+            weights = weight_variable([input_dim, output_dim])
+            variable_summaries(weights)
+        with tf.name_scope('biases'):
+            biases = bias_variable([output_dim])
+            variable_summaries(biases)
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            tf.summary.histogram('pre_activations', preactivate)
+        activations = act(preactivate, name='activation')
+        tf.summary.histogram('activations', activations)
+        return activations
 
 
 Maximum_layer_num = 5
 childbatch_size = 32
-childtrainbatchnum = int(np.ceil(1.0*len(train_x)/childbatch_size))
-childvalidbatchnum = int(np.ceil(1.0*len(dev_x)/childbatch_size))
+childtrainbatchnum = int(np.ceil(1.0 * len(train_x) / childbatch_size))
+childvalidbatchnum = int(np.ceil(1.0 * len(dev_x) / childbatch_size))
 
 with tf.Session(graph=tf.Graph()) as childsess:
     with tf.variable_scope("childDNN_", reuse=False):
         childinput_x = tf.placeholder(tf.float32, [None, len(features)], name="childinput_x")
         childinput_y = tf.placeholder(tf.int32, [None], name="childinput_y")
+        tf.summary.tensor_summary('child_x', childinput_x)
+        tf.summary.tensor_summary('child_y', childinput_y)
         childnsamples = childinput_y.shape[0]
         child_previous_layersize = len(features)
         childlayer = childinput_x
@@ -94,35 +130,16 @@ with tf.Session(graph=tf.Graph()) as childsess:
         # print tf.get_variable_scope()
         layer_sizes = []
         # child_params = []
-        np_chosen_classes = [10,10]
+        np_chosen_classes = [10, 20, 10]
         for time_step, chosen_class in enumerate(np_chosen_classes):
             layer_size = len(features) * (chosen_class + 1) / 10
             layer_sizes.append(layer_size)
-            childW = tf.Variable(tf.truncated_normal([child_previous_layersize, layer_size],
-                                                     stddev=1.0 / math.sqrt(float(child_previous_layersize))),
-                                 name="W" + str(time_step))
-            childb = tf.Variable(tf.zeros([layer_size]), name="b" + str(time_step))
 
-            variable_summaries(childW)
-            variable_summaries(childb)
-
-            # child_params.append(childW)
-            # child_params.append(childmul)
-            childlayer = tf.tanh(tf.matmul(childlayer, childW)+ childb)
-            #childlayer = tf.layers.dropout(childlayer)
+            childlayer = nn_layer(childlayer, child_previous_layersize, layer_size, 'layer' + str(time_step))
 
             child_previous_layersize = layer_size
 
-        childoutW = tf.Variable(tf.truncated_normal([child_previous_layersize, 2],
-                                                    stddev=1.0 / math.sqrt(float(child_previous_layersize))),
-                                name="Wout")
-        childoutb = tf.Variable(tf.zeros([2]), name="outb")
-
-        variable_summaries(childoutW)
-        variable_summaries(childoutb)
-
-        childscore = tf.matmul(childlayer, childoutW) + childoutb
-        #childprobability = tf.nn.softmax(childscore)
+        childscore = nn_layer(childlayer, child_previous_layersize, 2, 'layerout', act=tf.identity)
 
         # print(childscore, childinput_y)
         print layer_sizes
@@ -130,7 +147,7 @@ with tf.Session(graph=tf.Graph()) as childsess:
         childloglik = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=childscore, labels=childinput_y)
         # childloglik = tf.log(childprobability[tf.range(childbatch_size), childinput_y[:,0]])
 
-
+        tf.summary.tensor_summary('childloglik', childloglik)
 
         childloss = tf.reduce_mean(childloglik)
         tf.summary.scalar('loss', childloss)
@@ -144,7 +161,7 @@ with tf.Session(graph=tf.Graph()) as childsess:
 
         tf.summary.scalar('accuracy', childaccuracy)
         merged = tf.summary.merge_all()
-        train_writer = tf.summary.FileWriter('./train', childsess.graph)
+        train_writer = tf.summary.FileWriter('../train', childsess.graph)
         childsess.run(tf.global_variables_initializer())
 
         # child_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='childRNN')
@@ -156,17 +173,15 @@ with tf.Session(graph=tf.Graph()) as childsess:
                 childinput_y: train_y[0: childbatch_size]
             })'''
 
-
-
         child_acc_epoch = []
-        for childepoch in range(100):
+        for childepoch in range(50):
             batchloss = []
             for i in range(childtrainbatchnum):
                 summary, _, child_np_loss_value = childsess.run([merged, childtrainop, childloss], feed_dict={
                     childinput_x: train_x[i * childbatch_size: (i + 1) * childbatch_size],
                     childinput_y: train_y[i * childbatch_size: (i + 1) * childbatch_size]
                 })
-                #print 'batch ', i, 'loss:', child_np_loss_value
+                # print 'batch ', i, 'loss:', child_np_loss_value
                 batchloss.append(child_np_loss_value)
                 train_writer.add_summary(summary, i)
                 #  print 'Cost: ', child_np_loss_value
@@ -175,7 +190,8 @@ with tf.Session(graph=tf.Graph()) as childsess:
             childvalid_acc = predict(childsess, childinput_x, childinput_y, childaccuracy, dev_x, dev_y,
                                      childvalidbatchnum)
 
-            print 'Child Epoch', childepoch,'loss=',sum(batchloss)/len(batchloss), ': train ', childtrain_acc, 'valid ', childvalid_acc
+            print 'Child Epoch', childepoch, 'loss=', sum(batchloss) / len(
+                batchloss), ': train ', childtrain_acc, 'valid ', childvalid_acc
 
             child_acc_epoch.append(childvalid_acc)
 
